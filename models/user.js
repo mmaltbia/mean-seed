c76fed082d6d90df93e274da3cde7e76cf3d219c
Michelle Maltbia <maltbia.michelle@gmail.com>
2015-09-05 15:54:55 -0700

    Seed files added.

---
+++ models/user.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+});
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = User;
